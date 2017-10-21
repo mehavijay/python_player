5ab088639deaf2bb9bc9c959405cc13a47592a07
mehavijay <32951398+mehavijay@users.noreply.github.com>
2017-10-21 11:58:21 +0530

    Add files via upload

---
+++ strprgh.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse023
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse023 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+mystring=('programmer')
+print(len(mystring))
